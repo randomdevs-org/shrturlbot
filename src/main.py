7a10af02558d0edb4674893182679c21f0d0436f
ziotonak <ziotonak@gmail.com>
2022-12-10 14:17:12 +0100

    Setup logging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
+import json
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging(path=''):
+    """ Setup logging configuration from a json file """
+    global logger
+    with open(path) as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+    logger.info(f'Logger correctly initialized from {path}')
 
 
 def main():
-    pass
+    setup_logging('logging.json')
 
 
 if __name__ == '__main__':
